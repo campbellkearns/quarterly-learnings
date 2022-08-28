b36d121f6779b574977896a7afe68fb4d034b4ce
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2022-08-28 12:11:32 -0400

    Implements Dynamic post data rendering: wip

--- pages/blog.js
+++ pages/blog.js
@@ -1,5 +1,12 @@
+import Layout from '../components/layout'
+import { posts } from '../tmp/posts'
+
 export default function Blog() {
+
     return (
-        <h1>Some things I have been thinking about...</h1>
+        <Layout>
+            <h1>Some things I have been thinking about...</h1>
+            {posts.map(post => <h2>{post.Name}</h2>)}
+        </Layout>
     )
 }
