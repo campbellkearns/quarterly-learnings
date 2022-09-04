ab3faba79e3d1656c4f5ac1fd01d3148c67b39ed
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2022-09-04 17:51:40 -0400

    Adds build time post data retrieval, renders PostCards

--- pages/blog.js
+++ pages/blog.js
@@ -1,22 +1,33 @@
 import Layout from '../components/layout'
 import PostCard from '../components/PostCard'
 import styles from '../styles/Blog.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
 import { posts } from '../tmp/posts'
 // replace with imported function for retrieving filesystem post data
 
 
-export default function Blog() {
+export async function getStaticProps() {
+    const allPostsData = getSortedPostsData();
+
+    return {
+        props: {
+            allPostsData
+        }
+    }
+}
+
+export default function Blog({allPostsData}) {
 
     return (
         <Layout>
             <h1 className={styles.pageTitle}>Writings</h1>
             <section className={styles.grid}>
-                {posts.map(post => (
+                {allPostsData.map(post => (
                     <PostCard
-                        name={post.Name}
-                        date={new Date(post["Created On"]).toDateString()}
-                        imageUrl={post["Thumbnail image"]}
+                        name={post.title}
+                        date={new Date(post.dateCreated).toDateString()}
+                        imageUrl={post.imageUrl}
                     />)
                 )}
             </section>
