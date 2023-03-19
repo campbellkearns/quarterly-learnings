cd2448e420516016fa8f1377bc1f914a98019fb5
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-03-19 00:15:48 -0400

    Adds Tailwind and necessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'yellow': '#FFDB00',
+      'green': '#355834',
+      'green-dark': '#14281D',
+      'blue': '#80A1D4',
+      'brown': '#6E633D',
+    },
+  },
+  plugins: [],
+}
