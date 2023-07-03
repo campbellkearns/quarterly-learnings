012e15d65db6711ac6cf2ad501190ba30251075b
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-07-02 21:21:37 -0400

    Revert "Contact form"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        colors: {
-          primary: "#80A1D4",
-          secondary: "#355834",
-          tertiary: "#14281D",
-          accent: "#FFDB00",
-          neutral: "#6E633D",
-        },
-        fontFamily : {
-          serif: ['Lora', 'serif'],
-          sans: ['Manrope', 'sans-serif']
-        }
-      }
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'yellow': '#FFDB00',
+      'green': '#355834',
+      'green-dark': '#14281D',
+      'blue': '#80A1D4',
+      'brown': '#6E633D',
     },
-    plugins: [
-      require('@tailwindcss/forms'),
-    ],
-};
+  },
+  plugins: [],
+}
