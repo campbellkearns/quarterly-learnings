23ea3223b84b1073112726ee6348d5879ac42748
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-07-02 21:13:53 -0400

    Escapes apostrophe

--- pages/success.js
+++ pages/success.js
@@ -6,7 +6,7 @@ const Success = () => {
         <Layout>
             <main className="col-span-full text-center">
                 <h1 className="text-3xl font-serif mb-4">Thank you for reaching out!</h1>
-                <p className="font-sans mb-4">We've received your message and will get back to you as soon as possible.</p>
+                <p className="font-sans mb-4">We&apos;ve received your message and will get back to you as soon as possible.</p>
                 <button className="text-accent hover:underline">
                     <Link href="/"> 
                     Back to home
