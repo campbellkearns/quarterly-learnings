5bd95add25efa9849eace3f3ec297240d2e7c07c
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-07-02 21:01:18 -0400

    Added tailwind forms classes

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -11,16 +11,16 @@ const ContactForm = () => {
                 <input type="hidden" name="form-name" value="contact" />
                 <div>
                     <label htmlFor="yourname" className="font-sans mb-1 block">Your Name:</label>
-                    <input type="text" name="name" id="yourname" className="w-full p-2 border rounded font-sans text-primary" />
+                    <input type="text" name="name" id="yourname" className="form-input rounded p-2 border font-sans text-primary" />
                 </div>
                 <div>
-                    <label htmlFor="youremail" className="font-sans mb-1 block">Your Email:</label>
-                    <input type="email" name="email" id="youremail" className="w-full p-2 border rounded font-sans text-primary" />
+                    <label htmlFor="youremail" className="font-sans mb-1 block">Your Email Address:</label>
+                    <input type="email" name="email" id="youremail" className="form-input rounded p-2 border font-sans text-primary" />
                 </div>
 
                 <div>
                     <label htmlFor="yourmessage" className="font-sans mb-1 block">Message:</label>
-                    <textarea name="message" id="yourmessage" className="w-full p-2 border rounded font-sans text-primary" rows="6"></textarea>
+                    <textarea name="message" id="yourmessage" className="form-textarea w-full p-2 border rounded font-sans text-primary" rows="6"></textarea>
                 </div>
 
                 <button type="submit" className="bg-primary text-white px-4 py-2 rounded hover:bg-accent hover:text-primary">Send Message</button>
