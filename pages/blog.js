3c5fdf14fab7c12fdd0b8b27727d0adf5c06058a
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-07-06 22:02:55 -0400

    Removes module-based styling and begins post link layout

--- pages/blog.js
+++ pages/blog.js
@@ -1,41 +1,35 @@
-import Layout from '../components/layout'
-import PostCard from '../components/PostCard'
-import styles from '../styles/Blog.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Layout from "../components/layout";
+import PostCard from "../components/PostCard";
+import { getSortedPostsData } from "../lib/posts";
 
-import Link from 'next/link'
+import Link from "next/link";
 // replace with imported function for retrieving filesystem post data
-
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
 
     return {
         props: {
-            allPostsData
-        }
-    }
+            allPostsData,
+        },
+    };
 }
 
 export default function Blog({ allPostsData }) {
-
     return (
         <Layout>
-            <h1 className={styles.pageTitle}>Writings</h1>
-            <section className={styles.grid}>
-                {allPostsData.map(post => (
-                    <Link href={`/posts/${post.id}`} key={post.id}>
-                        <a>
-                            <PostCard
-                                name={post.title}
-                                date={new Date(post.dateCreated).toDateString()}
-                                imageUrl={post.imageUrl}
-                            />
-                        </a>
+            <h1 className="col-span-3 text-2xl">Writings</h1>
+            {allPostsData.map((post) => (
+                <article className="col-span-2" key={post.id}>
+                    <Link href={`/posts/${post.id}`}>
+                        <PostCard
+                            name={post.title}
+                            date={new Date(post.dateCreated).toDateString()}
+                            imageUrl={post.imageUrl}
+                        />
                     </Link>
-                )
-                )}
-            </section>
+                </article>
+            ))}
         </Layout>
-    )
+    );
 }
